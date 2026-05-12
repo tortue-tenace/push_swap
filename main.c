a923de295d76094824367ca9902c659299d42992
thattal <thattal@f4r1s6.paris.42.school>
2026-05-12 18:15:21 +0200

    aaqqq

--- main.c
+++ main.c
@@ -6,15 +6,65 @@
 /*   By: thattal <thattal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 14:53:27 by thattal           #+#    #+#             */
-/*   Updated: 2026/05/12 15:48:12 by thattal          ###   ########.fr       */
+/*   Updated: 2026/05/12 17:56:10 by thattal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\v' || c == '\f' || c == '\r');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && is_space(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+static int	has_duplicate(t_list *lst, int val)
+{
+	while (lst)
+	{
+		if (lst->data == val)
+			return (1);
+		lst = lst->next;
+	}
+	return (0);
+}
+
+static int	is_sorted(t_list *a)
+{
+	while (a && a->next)
+	{
+		if (a->data > a->next->data)
+			return (0);
+		a = a->next;
+	}
+	return (1);
+}
+
 int	main(int ac, char **ag)
 {
-	if (ac <= 1)
-		return (0);
-	
 }
